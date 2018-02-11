1f212068f6c8d96c81ad09df7c99ef80a3371d3a
GiantFrog <35314154+GiantFrog@users.noreply.github.com>
2018-02-11 13:55:45 -0700

    Fix cleanup_title method

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -10,32 +10,52 @@
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "GiantFrog"
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
-	puts "Processing File.... "
+  puts "Processing File.... "
 
-	begin
-		IO.foreach(file_name, encoding: "utf-8") do |line|
-			# do something for each line
-		end
+  begin
+    if RUBY_PLATFORM.downcase.include? 'mswin'
+      file = File.open(file_name)
+      unless file.eof?
+        file.each_line do |line|
+          # do something for each line (if using windows)
+          cleanup_title(line)
+        end
+      end
+      file.close
+    else
+      IO.foreach(file_name, encoding: "utf-8") do |line|
+        # do something for each line (if using macos or linux)
+      end
+    end
 
-		puts "Finished. Bigram model built.\n"
-	rescue
-		STDERR.puts "Could not open file"
+    puts "Finished. Bigram model built.\n"
+  rescue
+    STDERR.puts "Could not open file"
 		exit 4
-	end
+  end
 end
 
 # Makes extracted titles readable
 def cleanup_title(title)
-	title.slice!(/.*<SEP>/)				#gets title and tosses all other data
-	title.slice!(/([\(\[\{\\\/_\-:"`+=*]|feat\.).*/)#trims anything off the title after certain symbols
-	title.slice!(/[\?¿!¡.;&@%#\|]/) 		#removes some other symbols
-	title.slice!(/[^\w|\s]/)			#removes everything besides english letters and whitespace, which should
-	title.downcase!					#get rid of the earlier symbols too, but whatever.
-	return title
+	title.slice!(/.*<SEP>/)                           #gets title and tosses all other data
+	title.slice!(/([\(\[\{\\\/_\-:"`+=*]|feat\.).*/)  #trims anything off the title after certain symbols
+	dirty = true
+	while dirty
+		if not title.slice!(/[\?¿\!¡\.;&@%#\|"]/) 		  #my "global search" keeps slicing off characters until
+			dirty = false																	#the title is clean, then moves on.
+		end
+	end
+
+	if title.match(/[^\w|\s']/)                       #if the title has weird characters, toss it
+		return nil
+	else
+		title.downcase!																	#otherwise, return it in lowercase
+		return title
+  end
 end
 
 # Executes the program
